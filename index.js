46a43e519e87f9e4c88c15f234bd3c6b89d25aa4
Dhruv <dhruvchitkara27@gmail.com>
2019-02-04 04:14:05 +0530

    Connecting mongoose

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import parser from 'body-parser';
+import mongoose, {Schema} from 'mongoose';
+mongoose.connect('mongodb://user:chitkara27@ds221155.mlab.com:21155/node-express-jwt-test');
+const userSchema = new Schema({
+  username: {type: String, required: true, index: { unique: true }}
+});
+
 
 const app = new express();
 app.use(parser.urlencoded({
@@ -8,6 +14,10 @@ app.use(parser.urlencoded({
 
 app.get('/',(req,res) => {
   return res.send('Hello world');
+});
+
+app.get('/world', (req,res) => {
+  return res.send('World!');
 });
 
 app.post('/',(req,res) => {
