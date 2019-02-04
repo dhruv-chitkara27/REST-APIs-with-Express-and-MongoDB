b002f404459284b3b0636688102af07237d214e2
Dhruv <dhruvchitkara27@gmail.com>
2019-02-04 12:03:01 +0530

    Cleanup

---
+++ database/mongodb.js
@@ -0,0 +1,4 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://user:chitkara27@ds221155.mlab.com:21155/node-express-jwt-test');
+export default mongoose;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import parser from 'body-parser';
+import apiRoutes from './routes/api';
 import mongoose, {Schema} from 'mongoose';
+
+
 mongoose.connect('mongodb://user:chitkara27@ds221155.mlab.com:21155/node-express-jwt-test');
 const userSchema = new Schema({
   username: {type: String, required: true, index: { unique: true }}
@@ -12,25 +15,11 @@ const app = new express();
 app.use(parser.urlencoded({
   extended: false
 }));
+app.use('/api', apiRoutes);
 
 app.get('/',(req,res) => {
   return res.send('Hello world');
 });
 
-app.get('/users', (req,res) => {
-  User.find({}, function(err, users) {
-    return res.send(users);
-  });
-});
-
-app.post('/',(req,res) => {
-  const { username } = req.body;
-  const newUser = new User({
-    username: username
-  });
-  return newUser.save(function(err, model) {
-    return res.send(model);
-  });
-});
 
 app.listen(process.env.PORT || 3000);
